927da11965e5220e2b1c5ae109535942da81f1a3
Deng Changdong <dengchangdong@gmail.com>
2024-05-26 13:41:44 +0800

    Update Hero.vue

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -41,7 +41,7 @@ const { title, description } = useAppConfig()
           size="lg"
           type="outline"
           rel="noopener"
-          href="https://github.com/ccbikai/sink"
+          href="https://github.com/dengchangdong/sink"
           class="flex items-center justify-center gap-1"
           target="_blank"
         >
