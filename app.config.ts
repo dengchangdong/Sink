96733a95e6439e584869c88b5aedbe5b3f608736
Deng Changdong <dengchangdong@gmail.com>
2024-05-26 13:40:58 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
-  title: 'Sink',
+  title: 'Pin',
   description: 'A Simple / Speedy / Secrue Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  image: 'https://pin.ci/banner.png',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
